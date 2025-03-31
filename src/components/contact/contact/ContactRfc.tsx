b89c434a454f4147186e213c8dcc97b457b93627
AlimMah <142705560+AlimMahmudov@users.noreply.github.com>
2025-03-31 09:12:29 +0600

    dev (#5)

--- src/components/contact/contact/ContactRfc.tsx
+++ src/components/contact/contact/ContactRfc.tsx
@@ -1,48 +1,41 @@
-import { useProposalMutation } from "@/redux/api/contact";
+import axios from "axios";
 import { useTranslations } from "next-intl";
-import React from "react";
+import React, { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
-type ContactFormData = {
-  id: number;
+interface IFormInput {
   client_name: string;
   email: string;
-  object_choices: number;
+  object_choices: string;
   message: string;
-};
+}
+
+interface FetchDataPro extends IFormInput {
+  id: number;
+}
 
 const ContactRfc = () => {
-  const { register, handleSubmit, reset } = useForm<ContactFormData>();
-  const [contactDataRfc] = useProposalMutation();
   const t = useTranslations("Contact");
+  const [val, setVal] = useState<FetchDataPro[]>([]);
+  const { register, handleSubmit, reset } = useForm<IFormInput>();
+  // const [loading, setLoading] = useState(false);
 
-  const onSubmit: SubmitHandler<ContactFormData> = async (data) => {
+  console.log(val);
+
+  const onSubmit: SubmitHandler<IFormInput> = async (formData) => {
     try {
-      const { object_choices, ...restData } = data;
-      const contactPayload = {
-        ...restData,
-        object_choices: object_choices.toString(),
-      };
-
-      console.log("RFC", contactPayload);
-
-      const { data: responseData, error } = await contactDataRfc(
-        contactPayload
+      const { data } = await axios.post<FetchDataPro>(
+        "https://kazshar.site/ru/api/kashgar/rfc_proposal/",
+        formData
       );
-
-      if (responseData) {
-        localStorage.setItem("tokens", JSON.stringify(responseData));
-        reset();
-        alert("Сообщение отправлено!");
-      } else {
-        const errorMessage = error as { data: { message: string } };
-        console.error("Error response:", errorMessage);
-        alert(errorMessage.data.message || "Unknown error");
-      }
+      setVal((prev) => [...prev, data]); // Добавляем только подтвержденные сервером данные
+      reset();
     } catch (e) {
-      console.error("Unexpected error:", e);
+      console.error(e);
+      alert("Ошибка при отправке");
     }
   };
+
   return (
     <div className="">
       <div className="flex flex-col items-center  gap-1   max-w-[350px] w-full">
@@ -76,9 +69,9 @@ const ContactRfc = () => {
               {...register("object_choices", { required: true })}
               defaultValue=""
             >
-              <option value="0">{t("option")}</option>
-              <option value="1">{t("option1")}</option>
-              <option value="2">{t("option2")}</option>
+              <option value={4}>{t("option")}</option>
+              <option value={5}>{t("option1")}</option>
+              <option value={6}>{t("option2")}</option>
             </select>
           </div>
           <div className="flex text-white flex-col w-[100%] h-[100%]">
@@ -99,6 +92,59 @@ const ContactRfc = () => {
             </button>
           </div>
         </form>
+        {/* <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex items-start justify-center flex-col gap-1 max-w-[450px] w-full"
+        >
+          <div className="flex text-white flex-col w-[100%] h-[100%]">
+            <p className="text-[16px] font-[400]">{t("name")}</p>
+            <input
+              {...register("client_name", { required: "Это поле обязательно" })}
+              className="w-[100%] h-[32px] px-2 text-[14px] text-[#383838] outline-none"
+              type="text"
+            />
+          </div>
+          <div className="flex text-white flex-col w-[100%] h-[100%]">
+            <p className="text-[16px] font-[400]">{t("email")}</p>
+            <input
+              {...register("email", { required: "Это поле обязательно" })}
+              className="w-[100%] h-[32px] px-2 text-[14px] text-[#383838] outline-none"
+              type="text"
+            />
+          </div>
+          <div className="flex text-white flex-col w-[100%] h-[100%]">
+            <p className="text-[16px] font-[400]">{t("object")}</p>
+            <select
+              {...register("object_choices", {
+                required: true,
+                valueAsNumber: true,
+              })}
+              defaultValue={4}
+              className="w-[100%] h-[32px] px-2 text-[14px] bg-white text-[#383838] outline-none"
+            >
+              <option value={4}>{t("option")}</option>
+              <option value={5}>{t("option1")}</option>
+              <option value={6}>{t("option2")}</option>
+            </select>
+          </div>
+          <div className="flex text-white flex-col w-[100%] h-[100%]">
+            <p className="text-[16px] font-[400]">{t("message")}</p>
+            <textarea
+              {...register("message", { required: "Это поле обязательно" })}
+              className="w-[100%] h-[70px] px-2 py-1 text-[14px] text-[#383838] outline-none"
+            />
+          </div>
+          <div className="flex text-white flex-col w-[100%] h-[100%]">
+            <button
+              type="submit"
+              style={{ transition: "0.3s" }}
+              className="bg-[#c71212] mt-4 w-full border-[1px] border-white hover:bg-white hover:text-[#c71212] text-[#fff] font-[600] h-[32px]"
+              disabled={loading}
+            >
+              {loading ? "Отправка..." : t("btn3")}
+            </button>
+          </div>
+        </form> */}
       </div>
     </div>
   );
