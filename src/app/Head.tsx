add50b95a2b4dd43c94c5f9db1b4990adda2100c
AsimMahmudov <mahmudovasim799@gmail.com>
2025-05-12 17:54:34 +0600

    yandex

--- src/app/Head.tsx
+++ src/app/Head.tsx
@@ -16,7 +16,7 @@ const Head = () => {
           content="fQ0IqJz2eTDIDxVnU7cEV8OYPUrHscc9wXzSRQaozgY"
         />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" /> 
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -55,6 +55,41 @@ const Head = () => {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-17044270906');
+          `,
+        }}
+      />
+
+<Script
+        id="yandex-metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(101770548, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `,
+        }}
+      />
+
+<noscript
+        dangerouslySetInnerHTML={{
+          __html: `
+            <div>
+              <img src="https://mc.yandex.ru/watch/101770548" style="position:absolute; left:-9999px;" alt="" />
+            </div>
           `,
         }}
       />
--- src/constants/seo/YandexMetrika.tsx
+++ src/constants/seo/YandexMetrika.tsx
@@ -1,43 +1,44 @@
-"use client";
-
-import Image from "next/image";
-import { useEffect } from "react";
+import Script from "next/script";
 
 const YandexMetrika = () => {
-	useEffect(() => {
-		const script = document.createElement("script");
-		script.type = "text/javascript";
-		script.innerHTML = `
-      (function(m,e,t,r,i,k,a){
-         m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-         m[i].l=1*new Date();
-         for (var j = 0; j < document.scripts.length; j++) {
-           if (document.scripts[j].src === r) return;
-         }
-         k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-         k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-       })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+  return (
+    <>
+      <Script
+        id="yandex-metrika"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) return;
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-       ym(101542905, "init", {
-         clickmap:true,
-         trackLinks:true,
-         accurateTrackBounce:true
-       });
-    `;
-		document.head.appendChild(script);
-	}, []);
+            ym(101770548, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `,
+        }}
+      />
 
-	return (
-		<noscript>
-			<div>
-				<Image
-					src="https://mc.yandex.ru/watch/101542905"
-					style={{ position: "absolute", left: "-9999px" }}
-					alt=""
-				/>
-			</div>
-		</noscript>
-	);
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `
+            <div>
+              <img src="https://mc.yandex.ru/watch/101770548" style="position:absolute; left:-9999px;" alt="" />
+            </div>
+          `,
+        }}
+      />
+    </>
+  );
 };
 
 export default YandexMetrika;
