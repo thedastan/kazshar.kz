74ada58c36c61391a7f444612d62e7ae62e18343
Dastan Mukeev <55836145+thedastan@users.noreply.github.com>
2025-08-18 01:05:51 +0600

    Update Footer.tsx

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
 		<footer className="bg-[#1b1b1b] py-3">
 			<div className="flex md:flex-row flex-col text-center justify-between container text-white">
 				<Description>{t("text1")}</Description>
-				<Description>{t("text2")}</Description>
+				<a href="https://bir.kg" target="_blank"><Description>{t("text2")}</Description></a>
 			</div>
 		</footer>
 	);
